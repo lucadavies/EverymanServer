8ca9fad305b41c2773720be9aaad221bea0fef15
Luca Davies <luca-davies@outlook.com>
2021-11-19 19:52:28 +0000

    Improvements on autogen

--- Server.java
+++ Server.java
@@ -1,8 +1,8 @@
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.BufferedReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,8 +11,8 @@ import java.time.format.DateTimeFormatter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.io.Writer;
 import java.net.InetSocketAddress;
+import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -79,13 +79,14 @@ public class Server
             paths.filter(Files::isRegularFile)
             .filter(f -> f.getFileName().toString().toLowerCase().endsWith(".mp3"))
             .forEach(f -> ReadMp3(f));
-
-            CreateLibraryPage("library.html", tracks);
         }
         catch (IOException e)
         {
             System.out.println("Failed to read music files.");
         }
+
+        CreateLibraryPage("library.html", tracks);
+        CreatePlayPages(tracks);
     }
 
     private void ReadMp3(Path filepath)
@@ -115,14 +116,15 @@ public class Server
                     fos.write(coverArt.getBinaryData());
                 }
             }
-            if (!playPage.exists())
-            {
-                String fileName = playPage.getName();
-                String fileNameBase = fileName.substring(0, fileName.length() - 4);
-                CreateMusicPage(fileNameBase, trackTitle, albumTitle);
-            }
 
-            tracks.add(new TrackInfo(trackTitle, albumTitle, trackLength, albumArtFile.toPath().toString(), trackTitle));
+            tracks.add(new TrackInfo(
+                trackTitle,
+                albumTitle,
+                trackLength,
+                "music/" + playPage.getName().substring(0, playPage.getName().length() - 4) + ".html",
+                "res/library/" + filepath.getFileName().toString(),
+                "res/library/" + albumArtFile.getName()
+            ));
         }
         catch (Exception e)
         {
@@ -131,47 +133,80 @@ public class Server
         }
     }
 
-    private void CreateMusicPage(String fileName, String title, String album) throws IOException
+    private void CreatePlayPages(ArrayList<TrackInfo> tracks)
     {
-        FileWriter writer = new FileWriter("music/" + fileName + ".html");
-        writer.append("<!DOCTYPE html>");
-        writer.append("\n<html>");
-        writer.append("\n  <body>");
-        writer.append("\n    <h2>" + title + "</h2>");
-        writer.append("\n    <h3>" + album + "</h3>");
-        writer.append("\n    <image src=\"../res/library/" + album + ".jpg\">");
-        writer.append("\n    <audio id=\"music\" controls>");
-        writer.append("\n      <source src=\"../res/library/" + fileName + ".mp3\" type=\"audio/mpeg\">");
-        writer.append("\n      Your browser does not supprt HTML5 audio.");
-        writer.append("\n    </audio>");
-        writer.append("\n  </body>");
-        writer.append("\n</html>");
-        writer.close();
-    }
-
-    private void CreateLibraryPage(String fileName, ArrayList<TrackInfo> tracks) throws IOException
-    {
-        FileWriter writer = new FileWriter(fileName);
-        writer.append("<!DOCTYPE html>");
-        writer.append("\n<html>");
-        writer.append("\n  <body>");
-        writer.append("\n    <table>");
-        writer.append("\n      <tbody>");
+        try
+        {
+            FileWriter writer = null;
+            File f;
         for (TrackInfo track : tracks)
         {
-            writer.append("\n      <tr>");
-            writer.append("\n        <td><image src=\"" + track.getArtworkPath() + "\"></td>");
-            writer.append("\n        <td>" + track.getTitle() + "</td>");
-            writer.append("\n        <td>" + track.getAlbumTitle() + "</td>");
-            writer.append("\n        <td>" + track.getLength() + "s</td>");
-            writer.append("\n        <td><a href=\"" + track.getPagePath() + "\">Play</a></td>");
-            writer.append("\n      </tr>");
+            f = new File(track.getPagePath());
+            if (f.createNewFile())
+            {
+                writer = new FileWriter(track.getPagePath());
+                writer.append("<!DOCTYPE html>");
+                writer.append("\n<html>");
+                writer.append("\n  <body>");
+                writer.append("\n    <h2>" + track.getTitle() + "</h2>");
+                writer.append("\n    <h3>" + track.getAlbumTitle() + "</h3>");
+                writer.append("\n    <image src=\"/" + track.getArtworkPath() + "\">");
+                writer.append("\n    <p>");
+                writer.append("\n      <audio id=\"/res/library\" controls>");
+                writer.append("\n        <source src=\"/" + track.getAudioPath() + "\" type=\"audio/mpeg\">");
+                writer.append("\n        Your browser does not supprt HTML5 audio.");
+                writer.append("\n      </audio>");
+                writer.append("\n    </p>");
+                writer.append("\n    <p>");
+                writer.append("\n      <a href=\"/library.html\">Return to Library</a>");
+                writer.append("\n    </p>");
+                writer.append("\n  </body>");
+                writer.append("\n</html>");
+            }
+
+            if (writer != null)
+            {
+                writer.close();
+            }
         }
-        writer.append("\n      </tbody>");
-        writer.append("\n    </table>");
-        writer.append("\n  </body>");
-        writer.append("\n</html>");
-        writer.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println("Error creating play pages: " + e.getMessage());
+        }
+        
+    }
+
+    private void CreateLibraryPage(String fileName, ArrayList<TrackInfo> tracks)
+    {
+        try
+        {
+            FileWriter writer = new FileWriter(fileName);
+            writer.append("<!DOCTYPE html>");
+            writer.append("\n<html>");
+            writer.append("\n  <body>");
+            writer.append("\n    <table>");
+            writer.append("\n      <tbody>");
+            for (TrackInfo track : tracks)
+            {
+                writer.append("\n      <tr>");
+                writer.append("\n        <td><image src=\"" + track.getArtworkPath() + "\"></td>");
+                writer.append("\n        <td>" + track.getTitle() + "</td>");
+                writer.append("\n        <td>" + track.getAlbumTitle() + "</td>");
+                writer.append("\n        <td>" + track.getLength() + "s</td>");
+                writer.append("\n        <td><a href=\"" + track.getPagePath() + "\">Play</a></td>");
+                writer.append("\n      </tr>");
+            }
+            writer.append("\n      </tbody>");
+            writer.append("\n    </table>");
+            writer.append("\n  </body>");
+            writer.append("\n</html>");
+            writer.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println("Error creating Library page: " + e.getMessage());
+        }
     }
 
     //#region Helper Methods
@@ -253,6 +288,7 @@ public class Server
         {
             this.t = httpEx;
             String method = t.getRequestMethod();
+            String decodedURI = URLDecoder.decode(t.getRequestURI().toString(), StandardCharsets.UTF_8);
             String headers = "";
             for (Map.Entry<String, List<String>> h : t.getRequestHeaders().entrySet())
             {
@@ -263,22 +299,19 @@ public class Server
             {
             rHeaders += (h + "\n");
             }
-            // System.out.println("Method: " + method);
-            // System.out.println("Headers:\n" + headers);
-            // System.out.println("RespHeaders: " + rHeaders);
 
             if (method.equals("GET"))
             {
-                String s = t.getRemoteAddress() + "| GET: " + t.getRequestURI().toString() + " (HanRoot)";
+                String s = t.getRemoteAddress() + "| GET: " + decodedURI + " (HanRoot)";
                 log(s);
                 System.out.println(s);
-                get(t.getRequestURI().toString());
+                get(decodedURI);
             } else if (method.equals("POST"))
             {
-                String s = t.getRemoteAddress() + "| POST: " + t.getRequestURI().toString() + " (HanRoot)";
+                String s = t.getRemoteAddress() + "| POST: " + decodedURI + " (HanRoot)";
                 log(s);
                 System.out.println(s);
-                post(t.getRequestURI().toString(), null);
+                post(decodedURI, null);
 
             }
             t.close();
@@ -358,24 +391,15 @@ public class Server
         {
             this.t = httpEx;
             String method =  t.getRequestMethod();
+            String decodedURI = URLDecoder.decode(t.getRequestURI().toString(), StandardCharsets.UTF_8);
            
             if (method.equals("GET"))
             {
-                String s = t.getRemoteAddress() + "| GET: " + t.getRequestURI().toString() + " (HanRes)";
+                String s = t.getRemoteAddress() + "| GET: " + decodedURI + " (HanRes)";
                 log(s);
                 System.out.println(s);
-                get(t.getRequestURI().toString());
-            }
-            else if (method.equals("POST"))
-            {
-                String s = t.getRemoteAddress() + "| POST: " + t.getRequestURI().toString() + " (HanRes)";
-                log(s);
-                System.out.println(s);
-                byte[] resp = resp404.getBytes();
-                t.getResponseHeaders().set("content-type", "text/html");
-                t.sendResponseHeaders(404, resp.length);
-                t.getResponseBody().write(resp);
-            }     
+                get(decodedURI);
+            }    
             t.close();
         }
 
@@ -406,13 +430,14 @@ public class Server
         {
             this.t = httpEx;
             String method = t.getRequestMethod();
+            String decodedURI = URLDecoder.decode(t.getRequestURI().toString(), StandardCharsets.UTF_8);
 
             if (method.equals("GET"))
             {
-                String s = t.getRemoteAddress() + "| GET: " + t.getRequestURI().toString() + " (HanMusic)";
+                String s = t.getRemoteAddress() + "| GET: " + decodedURI + " (HanMusic)";
                 log(s);
                 System.out.println(s);
-                get(t.getRequestURI().toString());
+                get(decodedURI);
             }
             t.close();
         }
--- TrackInfo.java
+++ TrackInfo.java
@@ -5,33 +5,27 @@ public class TrackInfo
     private String length;
     private String artworkPath;
     private String pagePath;
+    private String audioPath;
 
-    public TrackInfo(String title, String albumTitle, String length, String artworkPath, String pagePath)
+    public TrackInfo(String title, String albumTitle, String length, String pagePath, String audioPath, String artworkPath)
     {
         this.title = title;
         this.albumTitle = albumTitle;
         this.length = length;
-        this.artworkPath = artworkPath;
         this.pagePath = pagePath;
+        this.audioPath = audioPath;
+        this.artworkPath = artworkPath;
     }
 
-    public String getTitle() {
-        return title;
-    }
+    public String getTitle() { return title; }
 
-    public String getAlbumTitle() {
-        return albumTitle;
-    }
+    public String getAlbumTitle() { return albumTitle; }
 
-    public String getLength() {
-        return length;
-    }
+    public String getLength() { return length; }
 
-    public String getArtworkPath() {
-        return artworkPath;
-    }
+    public String getPagePath() { return pagePath; }
 
-    public String getPagePath() {
-        return pagePath;
-    }
+    public String getAudioPath() { return audioPath; }
+
+    public String getArtworkPath() { return artworkPath; }
 }
